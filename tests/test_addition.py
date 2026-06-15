94f579ca23ee89dc295d9b281db6a121e2e574a3
Ben <bennyp2494@gmail.com>
2026-06-15 02:56:39 -0600

    linted tests

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -4,6 +4,7 @@ import my_test_pypackage as my_test_pypackage
 def test_add_success():
     assert my_test_pypackage.my_add(1, 2) == 3
 
+
 def test_add_two():
     assert my_test_pypackage.my_add(4, 7) == 11
     assert my_test_pypackage.my_add(-3, 7) == 4
